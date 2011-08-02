13d24d45c5e1d9e3a04060df42aa4295fb1dba6a
Wes Williams <wes.williamz@gmail.com>
2011-08-02 17:06:57 -0500

    added test and fixed issue with redirect on failure after add

--- spec/list_controller_spec.js
+++ spec/list_controller_spec.js
@@ -4,14 +4,23 @@ var controller = require('../controllers/list.js'),
     dummyList = {name: 'test', products: [], add: emptyFunc, save: emptyFunc},
     dummyResData = { 
       title: 'One List to Rule Them All',
-      list: dummyList};
+      list: dummyList},
+    failSave = function(callback) {
+      callback('error', dummyList);
+    },
+    successSave = function(callback) { callback(false, dummyList); };
+
+var spySave = function(save) {
+  spyOn(dummyList, 'save').andCallFake(save);
+};
 
 beforeEach(function() {
   mockReq = {param: emptyFunc};
   spyOn(mockReq, 'param').andReturn({name: 'myproduct'});
 
-  mockRes = {render: emptyFunc};
+  mockRes = {render: emptyFunc, redirect: emptyFunc};
   spyOn(mockRes, 'render').andCallFake(emptyFunc);
+  spyOn(mockRes, 'redirect').andCallFake(emptyFunc);
 
   mockList = jasmine.createSpy('mockList');
   mockList.findByUser = function(user, callback) {
@@ -29,19 +38,13 @@ describe('list controller', function() {
   });
 
   it('should render the list on successfully adding a product', function() {
-    spyOn(dummyList, 'save').andCallFake(function(callback) {
-      callback(false, dummyList);
-    });
+    spySave(successSave);
     myController.add(mockReq, mockRes);
     expect(mockRes.render).toHaveBeenCalledWith('list',dummyResData);
   });
 
   it('should redirect to root on error adding a product', function() {
-    mockRes.redirect = emptyFunc;
-    spyOn(mockRes, 'redirect').andCallFake(emptyFunc);
-    spyOn(dummyList, 'save').andCallFake(function(callback) {
-      callback(true, dummyList);
-    });
+    spyOn(dummyList, 'save').andCallFake(failSave);
     myController.add(mockReq, mockRes);
     expect(mockRes.redirect).toHaveBeenCalledWith('/list');
   });
@@ -50,11 +53,7 @@ describe('list controller', function() {
     mockList.findByUser = function(user, callback) {
       callback(true, dummyList);
     };
-    mockRes.redirect = emptyFunc;
-    spyOn(mockRes, 'redirect').andCallFake(emptyFunc);
-    spyOn(dummyList, 'save').andCallFake(function(callback) {
-      callback(true, dummyList);
-    });
+    spyOn(dummyList, 'save').andCallFake(failSave);
     myController.add(mockReq, mockRes);
     expect(mockRes.redirect).toHaveBeenCalledWith('/list');
   });
